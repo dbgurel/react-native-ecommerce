acf7545e66ee6d086892e487a40d159fd32ffce7
fatihkarayel <muhammetfatihkarayel@gmail.com>
2021-12-17 14:25:09 +0300

    validation eklendi

--- src/Screens/SupplierList/SupplierForm.js
+++ src/Screens/SupplierList/SupplierForm.js
@@ -1,11 +1,29 @@
 import { Formik } from 'formik'
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 import { Button } from 'react-native-elements'
-
+import ProductContext from '../../context/ProductContext'
+import * as yup from 'yup'
+import { string } from 'yup/lib/locale'
 
 
 const NewProductForm = ({ navigation }) => {
+    const { addedProduct, setAddedProduct } = useContext(ProductContext)
+
+    const formValidationSchema = yup.object().shape({
+        name: yup.string()
+            .required('Name is required'),
+
+        id: yup.number()
+            .required('ID is required'),
+
+        unitPrice: yup.number()
+            .required('Price is required'),
+
+        unitsInStock: yup.number()
+            .required('Stock information is required')
+    })
+
 
     const submitForm = (values) => {
 
@@ -38,28 +56,28 @@ const NewProductForm = ({ navigation }) => {
                         style={styles.input}
                         placeholder='Enter a companyName...'
                     />
-
+                     {errors.companyName && <Text style={styles.errorMessage}>{errors.companyName}</Text>}
                     <TextInput
                         onChangeText={handleChange('id')}
                         value={values.id}
                         style={styles.input}
                         placeholder='Enter an ID number...'
                     />
-
+                     {errors.id && <Text style={styles.errorMessage}>{errors.id}</Text>}
                     <TextInput
                         onChangeText={handleChange('contactName')}
                         value={values.contactName}
                         style={styles.input}
                         placeholder='Enter a Contact Name...'
                     />
-
+                     {errors.contactName && <Text style={styles.errorMessage}>{errors.contactName}</Text>}
                     <TextInput
                         onChangeText={handleChange('contactTitle')}
                         value={values.contactTitle}
                         style={styles.input}
                         placeholder='Enter a Contact Title...'
                     />
-
+                     {errors.contactTitle && <Text style={styles.errorMessage}>{errors.contactTitle}</Text>}
                     <Button title='Add Product to the List' onPress={handleSubmit} style={styles.button} />
                 </View>
             )}
@@ -82,6 +100,11 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         margin: 5,
         borderRadius: 20,
+    },
+    errorMessage:{
+        fontSize:10,
+        color:'red',
+        marginHorizontal:20
     }
 })
 
--- src/Screens/SupplierList/SupplierList.js
+++ src/Screens/SupplierList/SupplierList.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, ScrollView, StyleSheet  } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { View, Text, ScrollView, StyleSheet, ActivityIndicator } from 'react-native'
 import { Button, Card } from 'react-native-elements'
 
-const SupplierList = ({navigation}) => {
+const SupplierList = ({ navigation }) => {
 
     const [supplierList, setSupplierList] = useState([])
-
+    const [fetchStatus, setFetchStatus] = useState(false)
     useEffect(() => {
 
         fetch('https://northwind.vercel.app/api/suppliers')
@@ -17,29 +17,58 @@ const SupplierList = ({navigation}) => {
     }, [])
 
     return (
-        <ScrollView>
-            {
-                supplierList && supplierList.map((item, key) => (
-                    <Card key={key}>
-                        <Card.Title>{item.name}</Card.Title>
-                        <Card.Divider />
-                        <View style={styles.cardContent}>
-                            <Text>Company Name: {item.companyName}</Text>
-                            <Text>Contact Name: {item.contactName}</Text>
-                            <Button title='Go To Detail' onPress={() => navigation.navigate('ProductDetail', {productItem : item} )} style={styles.button}/>
-                        </View>
-                    </Card>))
-            }
-        </ScrollView>
+        <> {
+            fetchStatus == false ? (<View style={styles.loadingContainer}><ActivityIndicator size="large" color="#0000ff" /></View>)
+
+                : (
+                    <ScrollView style={styles.container}>
+                        <View style={styles.addButtonContainer}><Button title='Add New Product' onPress={() => navigation.navigate('NewProductForm')} buttonStyle={styles.addButton} /></View>
+                        {
+                            supplierList && supplierList.map((item, key) => (
+                                <Card key={key}>
+                                    <Card.Title>{item.name}</Card.Title>
+                                    <Card.Divider />
+                                    <View style={styles.cardContent}>
+                                        <Text>Company Name: {item.companyName}</Text>
+                                        <Text>Contact Name: {item.contactName}</Text>
+                                        <Button title='Go To Detail' onPress={() => navigation.navigate('ProductDetail', { productItem: item })} style={styles.button} />
+                                    </View>
+                                </Card>))
+                        }
+                    </ScrollView>
+                )
+        }
+        </>
     )
 }
 
 const styles = StyleSheet.create({
     button: {
         width: 120,
-        marginTop:8
+        marginTop: 8,
+        alignSelf: 'center',
+    }, 
+    addButton: {
+        width: 200,
+        backgroundColor: 'tomato',
+    },
+    addButtonContainer: {
+        width: '100%',
+        marginTop: 15,
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     cardContent: {
+        alignItems: 'center'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     }
 })
