bfb291f6febf36b80da9c7520f26f5bb8f4a6314
fatihkarayel <muhammetfatihkarayel@gmail.com>
2021-12-21 16:48:46 +0300

    arama eklendi

--- src/Screens/SupplierList/SupplierList.js
+++ src/Screens/SupplierList/SupplierList.js
@@ -6,6 +6,9 @@ const SupplierList = ({ navigation }) => {
 
     const [supplierList, setSupplierList] = useState([])
     const [fetchStatus, setFetchStatus] = useState(false)
+    const [searchcompanyName, setSearchcompanyName] = useState('')
+    const [companynameSortToggle, setcompanyNameSortToggle] = useState(false)
+    const [idSortToggle, setIdSortToggle] = useState(false)
     useEffect(() => {
 
         fetch('https://northwind.vercel.app/api/suppliers/')
@@ -44,6 +47,41 @@ const SupplierList = ({ navigation }) => {
 
 
     }
+    const searchBycompanyName = (companyName) => {
+        companyName = companyName.toLowerCase()
+        setSearchcompanyName(companyName)
+        fetch('https://northwind.vercel.app/api/suppliers/')
+            .then((res) => res.json())
+            .then(data => {
+                var filteredProductsBycompanyName = data.filter(q => q.companyName.toLowerCase().includes(companyName))
+                setProductList(filteredProductsBycompanyName)
+            })
+    }
+
+    const sortSuppliersName = () => {
+        setcompanyNameSortToggle(!companynameSortToggle)
+        if (companynameSortToggle == true) {
+            let sortedProductsBycompanyName = _.orderBy(productList, ['companyname'], ['asc'])
+            setProductList(sortedProductsBycompanyName)
+        }
+        else {
+            let sortedProductsBycompanyName = _.orderBy(productList, ['companyname'], ['desc'])
+            setProductList(sortedProductsBycompanyName)
+        }
+    }
+    const sortid = () => {
+        setIdSortToggle(!idSortToggle)
+        if (idSortToggle == true) {
+            let sortedProductsById = _.orderBy(productList, ['Id'], ['asc'])
+            setProductList(sortedProductsById)
+        }
+        else {
+            let sortedProductsById = _.orderBy(productList, ['Id'], ['desc'])
+            setProductList(sortedProductsById)
+        }
+    }
+
+
     return (
 
         <> {
@@ -51,8 +89,20 @@ const SupplierList = ({ navigation }) => {
 
                 : (
                     <ScrollView style={styles.container}>
-                        <View style={styles.addButtonContainer}><Button title='Add New Product' onPress={() => navigation.navigate('NewProductForm')} buttonStyle={styles.addButton} />
+                        <View style={styles.addButtonContainer}>
+                            <Button title='Add New Product' onPress={() => navigation.navigate('NewProductForm')} buttonStyle={styles.addButton} />
                         </View>
+                        <View style={styles.sortContainer}>
+                                <Button title='Sort by Company Name' onPress={() => sortSuppliersName()} buttonStyle={styles.sortButton} />
+                                <Button title='Sort by Id' onPress={() => sortid()} buttonStyle={styles.sortButton} />
+                            </View>
+                            <View style={styles.searchContainer}>
+                                <TextInput
+                                    onChangeText={(e) => searchBycompanyName(e)}
+                                    style={styles.searchInput}
+                                    placeholder='Search by Comapny Name...'
+                                />
+                            </View>
                         {
                             supplierList && supplierList.map((item, key) => (
                                 <Card key={key}>
@@ -102,6 +152,31 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    sortContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 8,
+        marginHorizontal: 20,
+    },
+    searchContainer: {
+        flex: 1,
+        marginTop: 8
+    },
+    sortButton: {
+        flex: 1,
+        marginTop: 8,
+        width: 150,
+        backgroundColor: 'gray',
+    },
+    searchInput: {
+        padding: 5,
+        borderWidth: 1,
+        borderColor: 'pink',
+        margin: 3,
+        height: 50,
+        marginHorizontal: 20
     }
 })
 
